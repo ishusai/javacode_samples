d0466765c82823782a90cc487bb70c00cde5378b
Sai Iswarya <Sai Iswarya@LAPTOP-EJGFIJP4>
2023-04-20 11:39:03 +0530

    Initial commit

---
+++ sampletest/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Sai Iswarya
+ *
+ */
+module sampletest {
+}
---
+++ sampletest/src/sampletest/com/sampletest.java
@@ -0,0 +1,15 @@
+package sampletest.com;
+
+public class sampletest {
+
+	public static void main(String[] args) {
+		 
+			int a=0,b=1,c=0;
+			for(int i=1;i<=10;i++) {
+				c=a+b;
+				System.out.println(c);
+				a=b;
+				b=c;
+	}
+}
+	}
